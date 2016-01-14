50a1b9f8486d1e7b55d52ae881ed258df724ba28
Peter <peter.ivanics@reslink.fi>
2016-01-14 08:38:34 +0200

    Initial Commit

---
+++ CodeKataSwift/CodeKataSwift.h
@@ -0,0 +1,19 @@
+//
+//  CodeKataSwift.h
+//  CodeKataSwift
+//
+//  Created by Peter Ivanics on 14/01/16.
+//  Copyright © 2016 reslink. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CodeKataSwift.
+FOUNDATION_EXPORT double CodeKataSwiftVersionNumber;
+
+//! Project version string for CodeKataSwift.
+FOUNDATION_EXPORT const unsigned char CodeKataSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CodeKataSwift/PublicHeader.h>
+
+
